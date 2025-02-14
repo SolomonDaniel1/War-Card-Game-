d90dbf9477e0029fb8d6840a0ab5a5a9562eb4af
SolomonDaniel1 <prodmusic.official@gmail.com>
2025-02-14 08:30:35 +0530

    Add files via upload
    A digital version of the classic War card game, built using Swift and SwiftUI.In this game, two players compete by drawing cards from their respective decks, and the player with the higher card wins both. If the cards are tied, a "war" occurs, and additional cards are drawn until a winner is determined. The game continues until one player collects all the cards.

---
+++ War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Solomon Daniel on 14/02/25.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
